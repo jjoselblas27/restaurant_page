b69246e272dd4f60bb4e10036a0668c86d406251
jjose27 <juan.leandro@utec.edu.pe>
2021-10-03 03:48:18 -0500

    pequeños arreglos

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,17 @@
 const express = require('express');
 var app = express();
-app.get('/dihola', helloWorld);
-
-function helloWorld(req, res){
-    res.send("hola mundo");
-}
 
 const mysql = require('mysql');
 
-app.get('/muestra_datos', mostrar_menus);
+app.get('/menu', mostrar_menus);
+
 
 function mostrar_menus(req, res){
     //defino la conexion con la base de datos
     var connection = mysql.createConnection({
         host: 'localhost',
-        user: 'juanjlb',
-        password: 'hajime27',
+        user: '*****',
+        password: '*****',
         database: 'restaurant'
     });
 
