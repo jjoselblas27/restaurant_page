817c18f36246d1e9afe89bc6b083044c8290c795
jjose27 <juan.leandro@utec.edu.pe>
2021-10-09 00:36:24 -0500

    nuevos cambios

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,13 @@ var app = express();
 
 const mysql = require('mysql');
 
+
+app.get('/a',say_a);
+
+function say_a(req,res){
+    res.send("a");
+}
+
 app.get('/menu', mostrar_menus);
 
 
@@ -10,15 +17,15 @@ function mostrar_menus(req, res){
     //defino la conexion con la base de datos
     var connection = mysql.createConnection({
         host: 'localhost',
-        user: '*****',
-        password: '*****',
+        user: 'utec',
+        password: '1234567890',
         database: 'restaurant'
     });
 
     //establesco la conexion
     connection.connect();
 
-    var myQuery = "SELECT plato_id, nombre, precio, created_date, modified_date"+
+    var myQuery = "SELECT nombre, precio, created_date, modified_date"+
                   " FROM menu;";
     
     connection.query(myQuery, function(error,results, fields){
@@ -28,7 +35,7 @@ function mostrar_menus(req, res){
     });
 }
 
-//SELECT plato_id, nombre, precio, created_date, modified_date
+//SELECT  nombre, precio, created_date, modified_date
 //FROM menu;
 
 app.listen(3000, function(){
